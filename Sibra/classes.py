f7aed5fdcb7d3de96adfdeac260561e6b21344d6
Simon Guilbert <simonpolytech@gmail.com>
2020-02-03 12:20:05 +0100

    Méthode setArretsFils() fonctionnelle + menu user

--- Sibra/classes.py
+++ Sibra/classes.py
@@ -21,6 +21,11 @@ def right(s, amount):
 def mid(s, offset, amount):
     return s[offset:offset+amount]
 
+# =============================================================================
+# Date du jour
+# =============================================================================
+#Jour de la semaine d'aujoud'hui (0=lundi, 1=mardi, ..., 6=dimanche)
+jour = ("semaine" if date.today().weekday() < 5 else "weekend")
 
 # =============================================================================
 # Classes
@@ -33,24 +38,29 @@ class Ligne:
     def __init__(self,nomFichier,nomLigne):
         self.nom = nomLigne
         self.slited_content = sibra.lecture(nomFichier)
-        self.regular_path = self.slited_content[0].replace("+","N").split(" N ")
-        self.regular_date_go = sibra.dates2dic(self.slited_content[1])
-        self.regular_date_back = sibra.dates2dic(self.slited_content[2])
-        self.we_holidays_path = self.slited_content[3].replace("+","N").split(" N ")
-        self.we_holidays_date_go = sibra.dates2dic(self.slited_content[4])
-        self.we_holidays_date_back = sibra.dates2dic(self.slited_content[5])
+        self.path = self.slited_content[0 if jour == "semaine" else 3].replace("+","n").split(" n ")
+        self.date_go = sibra.dates2dic(self.slited_content[1 if jour == "semaine" else 4])
+        self.date_back = sibra.dates2dic(self.slited_content[2 if jour == "semaine" else 5])
+        
+    def getArretPrecedent(self,arret):
+        if arret == self.path[0]:
+            return None
+        return self.path[self.path.index(arret)-1]
+    
+    def getArretSuivant(self,arret):
+        if arret == self.path[len(self.path)-1]:
+            return None
+        return self.path[self.path.index(arret)+1]
+        
         
 class Arret:
-    def __init__(self, intitule, horairesSemaineGo, horairesSemaineBack, horairesWEGo, horairesWEBack, arretSuivantMemeLigne = None, arretSuivantAutreLigne = None):
+    def __init__(self, intitule, ligne, horairesGo, horairesBack):
         self.intitule = intitule
-        self.horairesSemaineGo = self.setHoraires(horairesSemaineGo)
-        self.horairesSemaineBack = self.setHoraires(horairesSemaineBack)
-        self.horairesWEGo = self.setHoraires(horairesWEGo)
-        self.horairesWEBack = self.setHoraires(horairesWEBack)
-        self.arretSuivantMemeLigne = arretSuivantMemeLigne
-        self.arretSuivantAutreLigne = arretSuivantAutreLigne
+        self.ligne = ligne
+        self.horairesGo = self.setHoraires(horairesGo)
+        self.horairesBack = self.setHoraires(horairesBack)
+        self.arretSuivants = []
         
-
     # Retourne la liste des horaires au format timedelta (obtenu avec la bibliotèque datetime)            
     def setHoraires(self,typeHoraires):
         l = []
@@ -75,28 +85,65 @@ class Arret:
     def getHorairesWEBack(self):
         return self.horairesWEBack
     
-    def getarretSuivantMemeLigne(self):
-        return self.arretSuivantMemeLigne
+    def getLigne(self):
+        return self.ligne
 
 
 class Reseau :
     def __init__(self,racine):
         self.racine = racine
+    
+#    def conditionArret(self,listeArrets):
+#        for arret in listeArrets:
+#            if arret[0] == arrivee:
+#                return True
+#        for arret in listeArrets:
+#            print("ici ",listeArrets)
+#            if arret[0] != None:
+#                return False
+#        return True
         
-    def setArretsFils(self,nomArretAAjouter,compteur,sens):
-        compteur += (1 if sens == "Go" else -1)
-        self.racine.arretSuivantMemeLigne = Arret(nomArretAAjouter,
-                                        regular_date_go[nomArretAAjouter],
-                                        regular_date_back[nomArretAAjouter],
-                                        we_holidays_date_go[nomArretAAjouter],
-                                        we_holidays_date_back[nomArretAAjouter])
-        print("actuel : ",self.racine.getIntitule())
-        print("suivant : ",self.racine.arretSuivantMemeLigne.getIntitule())
-        print("----")
-        if nomArretAAjouter == arrivee:
-            return "ok"
-        return Reseau(self.racine.arretSuivantMemeLigne).setArretsFils(semaineOuWE[compteur],compteur = compteur,sens = sens)
-            
+    def conditionArret(self):
+        return self.racine.getIntitule() == arrivee
+
+        
+    def croisementLigne(self,arret,ligne):
+        if arret not in intersections:
+            res = []
+            for l in listeLignes:
+                if l != ligne:
+                    if arret in l.path:
+                        res.append([l.getArretPrecedent(arret),l])
+                        res.append([l.getArretSuivant(arret),l])
+                        intersections.append(arret)
+            return ([[None]] if not res else res)
+        return [[None]]
+        
+                
+    def setArretsFils(self,arretsAAjouter):
+#        print("à ",self.racine.getIntitule())
+#        ex=input(self.racine.getLigne().nom)
+        for arret in arretsAAjouter:
+            if arret[0] != None:
+                nouvelArret = Arret(arret[0],
+                                    arret[1],
+                                    arret[1].date_go[arret[0]],
+                                    arret[1].date_back[arret[0]])
+                self.racine.arretSuivants.append(nouvelArret)
+                # Récursivité           .
+                if arret[1].getArretPrecedent(arret[0]) != self.racine.getIntitule():
+                    listeProchainsArrets = [[arret[1].getArretPrecedent(arret[0]),arret[1]]]
+                else:
+                    listeProchainsArrets = [[arret[1].getArretSuivant(arret[0]),arret[1]]]
+                listeProchainsArrets.extend(self.croisementLigne(arret[0],arret[1]))
+                print(self.racine.getIntitule(),len(self.racine.arretSuivants))
+#                print("-------------------------")
+#                print("de ",self.racine.getIntitule())
+#                print("de ",self.racine.getLigne().nom)
+                Reseau(nouvelArret).setArretsFils(listeProchainsArrets)
+
+
+        
 # =============================================================================
 # Lecture des fichiers d'origine
 # =============================================================================
@@ -106,87 +153,92 @@ listeFichiers = ['data/1_Poisy-ParcDesGlaisins.txt','data/2_Piscine-Patinoire_Ca
 listeLignes = []
 for fichier in listeFichiers:
     listeLignes.append(Ligne(fichier,mid(fichier,5,len(fichier)-8)))
-    
-def ajout(nomFichier):
-    if sibra.lecture(nomFichier) != 'Vide':
+
+#Ajoute un objet de classe Ligne à listeLignes à partir d'un autre fichier .txt   
+def ajouter(nomFichier):
+    if sibra.lecture(nomFichier) != ['vide']:
         listeFichiers.append(nomFichier)
+        listeLignes.append(Ligne(nomFichier,mid(nomFichier,5,len(nomFichier)-8)))
         print("Le fichier a bien été ajouté")
     else:
-        print("Attention : le fichier demandé n'a pas été trouvé")
+        print("\nAttention : le fichier demandé n'a pas été trouvé")
 
 # =============================================================================
 # Fonctions utilisées dans le programme principal    
 # =============================================================================
 def erreurSaisie(saisie):
     for ligne in listeLignes:
-        if saisie in semaineOuWE:
-            return [False,ligne]
-#    sleep(2)
+        if saisie in ligne.path:
+            return ligne
     return True
-
-        
+     
 # =============================================================================
 # Programme principal      
 # =============================================================================
-#Jour de la semaine d'aujoud'hui (0=lundi, 1=mardi, ..., 6=dimanche)
-jour = date.today().weekday()
-semaineOuWE = ("ligne.regular_path" if jour in (0,1,2,3,4) else "ligne.we_holidays_path" )
-
-messageDeDepart = "Voici la liste des fichiers qui seront traités pour calculer votre itinéraire. Si vous voulez en ajouter un autre, " + \
-    "entrez tout de suite la commande ajouter('nomDuFichier') dans la console. Si la liste des fichiers pris en compte " + \
-    "vous suffisent, entrez le nom de départ souhaité"
-depart = input(messageDeDepart + ', '.join([fichier for fichier in listeFichiers]))
-while left(depart,5) == "ajout":
-    depart = input(messageDeDepart + ', '.join([fichier for fichier in listeFichiers]))
- 
-while erreurSaisie(depart)[0] == True:
-    depart = (input("Le lieu de départ est introuvable. Réessayez.") if left(depart,5)!="ajout"
-              else input("Veuillez maintenant sélectionner votre lieu de départ")) 
-#    sleep(2)
-ligneDepart = erreurSaisie(depart)[1]
-
-arrivee = input("Veuillez choisir l'arrêt d'arrivée souhaité : ")
-while erreurSaisie(arrivee)[0] == True :
-    arrivee = input("Le lieu d'arrivee est introuvable. Réessayez.")  
-#    sleep(2)
-ligneArrivee = erreurSaisie(arrivee)[1]
-
-
-#depart = "GARE"
-#arrivee = "VIGNIERES"
+    
+#depart = "vernod"
+#arrivee = "ponchy"
+#ligneDepart = erreurSaisie(depart)
 ##mode = input("Comment voulez-vous circuler ? ")
-#
-#
-#
-#for ligne in listeLignes:
-#    if (depart and arrivee) in ligne.semaineOuWE:
-#        ligneDepart = ligne
-##        ligneArrivee = ligne
-#    elif depart in ligne.semaineOuWE:
-#        
-#        
-#
-#
-#if depart in regular_path:
-#    voyage = Reseau(Arret(depart,
-#                          regular_date_go[depart],
-#                          regular_date_back[depart],
-#                          we_holidays_date_go[depart],
-#                          we_holidays_date_back[depart]))
-#    if arrivee in regular_path:
-#        if regular_path.index(depart) < regular_path.index(arrivee):
-#            voyage.setArretsFils(regular_path[regular_path.index(depart)+1],regular_path.index(depart)+1,"Go")
-#        else:
-#            voyage.setArretsFils(regular_path[regular_path.index(depart)-1],regular_path.index(depart)-1,"Back")
-#            
-#             
+    
+# =============================================================================
+# Demande de saisie du lieu de départ
+# =============================================================================
+#"\033[4mChaîneDeCaractères\033[0m" renvoie ChaîneDeCaractères souligné dans la console
+print("\n\033[4mListe des fichiers déjà pris en compte:\033[0m")
+# Affichage des fichiers déjà pris en compte
+for fichier in listeFichiers:
+    print(fichier)
+print("\nCi-dessus la liste des fichiers qui seront traités pour calculer votre itinéraire. Si vous voulez en ajouter un autre, " + \
+    "entrez tout de suite le nom du fichier (.txt) à ajouter dans la console. Si la liste des fichiers pris en compte " + \
+    "vous suffisent, entrez le nom du départ souhaité.\n\n\033[4mVoici la liste des arrêts disponibles (ne pas mettre d'accents):\033[0m")
+
+affArrets = []
+for liste in listeLignes:
+    for arret in liste.path:
+        affArrets.append(arret)
+print(list(set(affArrets)))
+
+depart = input("Entrez le nom de l'arrêt de bus de départ ou du fichier .txt à ajouter : ").lower()
+
+while right(depart,4) == ".txt":
+    print("\nAjout en cours ...\n")
+    sleep(1)
+    ajouter(depart)
+    depart = input("Entrez le nom de l'arrêt de bus de départ ou du fichier .txt à ajouter : ").lower()
+    
+
+while erreurSaisie(depart) == True:
+    depart = input("Le lieu de départ est introuvable. Réessayez : ").lower()
+ligneDepart = erreurSaisie(depart)
+
+# =============================================================================
+# Demande de saisie du lieu d'arrivée
+# =============================================================================
+arrivee = input("Veuillez choisir l'arrêt d'arrivée souhaité : ")
+while erreurSaisie(arrivee) == True:
+    arrivee = input("Le lieu d'arrivee est introuvable. Réessayez : ").lower()
+
+# =============================================================================
+# Création de l'arborescence
+# =============================================================================
+# Liste initialement vide utilisée dans la fonction croisementLigne() de la classe Reseau
+intersections = []
+# Création du noeud racine de l'arborescence
+voyage = Reseau(Arret(depart,
+                      ligneDepart,
+                      ligneDepart.date_go[depart],
+                      ligneDepart.date_back[depart]))
+#Ajout de tous les autres arrêts de l'arborescence par la fonction récursive setArretsFils()
+voyage.setArretsFils([
+        [ligneDepart.getArretPrecedent(depart),ligneDepart],
+        [ligneDepart.getArretSuivant(depart),ligneDepart]])
+           
 ## =============================================================================
 ## Fin du programme principal    
 ## =============================================================================
 #
 #
-#print("\n\n\n ************** Affichages de tests *******************\n")
-#print(regular_path)
+print("\n\n\n ************** Affichages de tests *******************\n")
 
-                
-   
+
