74c311f0d9281e905c3d2185de99593173a059a9
Simon Guilbert <simonpolytech@gmail.com>
2020-02-04 20:10:50 +0100

    Update sibra.py

--- Sibra/sibra.py
+++ Sibra/sibra.py
@@ -4,7 +4,52 @@ Created on Thu Jan 23 08:23:04 2020
 
 @author: guilbers
 """
+import csv
+from datetime import datetime
 
+# =============================================================================
+# Fonctions trouvées sur internet pour simplifier le code
+# =============================================================================
+def left(s, amount):
+    return s[:amount]
+
+def right(s, amount):
+    return s[-amount:]
+
+def mid(s, offset, amount):
+    return s[offset:offset+amount]
+
+# =============================================================================
+# Jours fériés
+# =============================================================================
+joursFeriesVacances = []
+with open('data/jours-feries-seuls.csv', newline='') as csvfile:
+    csvfile.readline()
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+        joursFeriesVacances.append(datetime(
+                year = int(left(row[0],4)),
+                month = int(mid(row[0],5,2)),
+                day = int(right(row[0],2)))) 
+        
+# =============================================================================
+# Vacances scolaire 2020
+# =============================================================================
+with open('data/vacancesScolaires2020.csv', newline='') as csvfile:
+    csvfile.readline()
+    spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+    for row in spamreader:
+        joursFeriesVacances.append(datetime(
+                year = int(left(row[1],4)),
+                month = int(mid(row[1],5,2)),
+                day = int(right(row[1],2))))
+        
+def getJoursFeriesVacances():
+    return joursFeriesVacances
+        
+# =============================================================================
+# Lecture des fichiers relatifs aux lignes de bus
+# =============================================================================
 def lecture(data_file_name):
     try:
         with open(data_file_name, 'r') as f:
