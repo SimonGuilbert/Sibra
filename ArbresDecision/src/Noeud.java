6a0b8e68691cda341f4c68176131b44829758dcc
Simon Guilbert <simonpolytech@gmail.com>
2020-03-18 17:08:30 +0100

    Update Noeud.java

--- ArbresDecision/src/Noeud.java
+++ ArbresDecision/src/Noeud.java
@@ -1,21 +1,36 @@
 import java.util.ArrayList;
 
 public class Noeud{
-	private String nom = "Aucun";
 	private ArrayList<Noeud> fils = new ArrayList<Noeud>();
+	private String attributAssocie;
+	private String nom;
 	
-	protected String getNom() {
-		return this.nom;
+	// Constructeur
+	protected Noeud(String unNom) {
+		this.nom = unNom;
+	}
+	
+	protected String getAttribut() {
+		return this.attributAssocie;
 	}
 	
 	protected void setNom(String unNom) {
 		this.nom = unNom;
 	}
 	
+	protected String getNom() {
+		return this.nom;
+	}
+	
+	protected void setAttribut(String unAttribut) {
+		this.attributAssocie = unAttribut;
+	}
+	
 	protected ArrayList<Noeud> getFils(){
 		return this.fils;
 	}
 	
-	
-
+	protected void ajoutFils(Noeud unFils) {
+		this.fils.add(unFils);
+	}
 }
