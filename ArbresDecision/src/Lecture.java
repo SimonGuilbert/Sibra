d0c09a871ab42f335ded886169168dc6ff859cf2
Simon Guilbert <simonpolytech@gmail.com>
2020-03-18 17:09:03 +0100

    Update Lecture.java

--- ArbresDecision/src/Lecture.java
+++ ArbresDecision/src/Lecture.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -11,17 +12,17 @@ public class Lecture {
 		this.nomFichier = unNomFichier;	
 	}
 	
-	protected ArrayList<String []> data() throws IOException { //List<List<String>>
-		//List<List<String>> res = new ArrayList<List<String>>();
-		ArrayList<String []> res = new ArrayList<String []>();
+	protected ArrayList<ArrayList<String>> data() throws IOException {
+		ArrayList<ArrayList<String>> res = new ArrayList<ArrayList<String>>();
 		String row = "";
 		BufferedReader csvReader = new BufferedReader(new FileReader(this.nomFichier));
 		while ((row = csvReader.readLine()) != null) {
-		    String [] d = row.split(",");
-		    res.add(d);
+		    String [] temp = row.split(",");
+		    ArrayList<String> temp2 = new ArrayList<String>();		    
+		    Collections.addAll(temp2, temp); // Insère toutes les valeurs de temp dans temp2
+		    res.add(temp2);
 		}
 		csvReader.close();
 		return res;
-	}
-	
+	}	
 }
