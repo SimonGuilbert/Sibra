9e98f254587286a6a6f6eca9dee9bf07880cf28e
Simon Guilbert <simonpolytech@gmail.com>
2020-03-18 17:06:53 +0100

    Update Main.java

--- ArbresDecision/src/Main.java
+++ ArbresDecision/src/Main.java
@@ -2,14 +2,12 @@ import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
-		Lecture l = new Lecture("Data/golf.csv");
-		//Lecture l = new Lecture("Data/soybean-app.csv");
-		Noeud n = new Noeud();
-		l.data();
-		Arbre a = new Arbre(n,l.data());
-		System.out.println(n.getNom());
-		
-		
+		//Lecture l = new Lecture("Data/golf.csv");
+		Lecture l = new Lecture("Data/soybean-app.csv");
+		Arbre a = new Arbre(new Noeud("PremierNoeud"),l.data());
+		a.setFils();
+		System.out.println(a.racine.getFils().get(1).getFils().get(2).getNom());
+		System.out.println(a.racine.getFils().get(1).getFils().get(2).getAttribut());
 	}
 
 }
