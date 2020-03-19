980776e424c2fced6dd8532278557ad280c8b206
Simon Guilbert <simonpolytech@gmail.com>
2020-03-19 12:58:28 +0100

    Update Main.java

--- ArbresDecision/src/Main.java
+++ ArbresDecision/src/Main.java
@@ -6,8 +6,9 @@ public class Main {
 		Lecture l = new Lecture("Data/soybean-app.csv");
 		Arbre a = new Arbre(new Noeud("PremierNoeud"),l.data());
 		a.setFils();
-		System.out.println(a.racine.getFils().get(1).getFils().get(2).getNom());
-		System.out.println(a.racine.getFils().get(1).getFils().get(2).getAttribut());
+		//System.out.println(a.racine.getFils().get(0).getFils().get(0).getFils());
+		//System.out.println(a.racine.getFils().get(1).getFils().get(2).getAttribut());
+		System.out.println(a.getPredClasses());
 	}
 
 }
