726d875566d1a4b8e54f862333d426443247ab10
Simon Guilbert <simonpolytech@gmail.com>
2020-03-21 14:19:54 +0100

    Update Main.java

--- ArbresDecision/src/Main.java
+++ ArbresDecision/src/Main.java
@@ -2,13 +2,19 @@ import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
-		//Lecture l = new Lecture("Data/golf.csv");
-		Lecture l = new Lecture("Data/soybean-app.csv");
-		Arbre a = new Arbre(new Noeud("PremierNoeud"),l.data());
+		//Lecture lApp = new Lecture("Data/golf.csv");
+		// PARTIE APPRENTISSAGE
+		Lecture lApp = new Lecture("Data/soybean-app.csv");
+		Arbre a = new Arbre(new Noeud("PremierNoeud"),lApp.data());
 		a.setFils();
-		//System.out.println(a.racine.getFils().get(0).getFils().get(0).getFils());
-		//System.out.println(a.racine.getFils().get(1).getFils().get(2).getAttribut());
-		System.out.println(a.getPredClasses());
+		Matrice matApp = new Matrice(a.getPredClasses(),a.listeDerniersElements(),a.listeClasses);
+		matApp.afficher();
+		
+		// PARTIE PREDICTION
+		Lecture lPred = new Lecture("Data/soybean-pred.csv");
+		a.setDonnees(lPred.data()); // On change les données (nouvelles données = soybean-pred.csv)
+		Matrice matPred = new Matrice(a.getPredClasses(),a.listeDerniersElements(),a.listeClasses);
+		matPred.afficher();
 	}
 
 }
