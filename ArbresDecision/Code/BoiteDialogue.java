0f439b89adc3d149e025e1691d2034c760d392a4
Simon Guilbert <simonpolytech@gmail.com>
2020-03-29 03:14:01 +0200

    Commentaires

--- ArbresDecision/Code/BoiteDialogue.java
+++ ArbresDecision/Code/BoiteDialogue.java
@@ -21,13 +21,13 @@ public class BoiteDialogue extends JDialog implements ActionListener{
   public BoiteDialogue(){
 	this.setModal(true);
 	this.setTitle("Traitement des valeurs manquantes");
-    this.setSize(715, 255);
-    this.setLocationRelativeTo(null);
-    this.setResizable(false);
-    this.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+    this.setSize(715, 255); // En pixels
+    this.setLocationRelativeTo(null); // Au milieu de l'Ã©cran
+    this.setResizable(false); // Empeche la redimension
+    this.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE); // Ne se passe rien si on clique sur la croix rouge
     
-    //Phrase du début
-    JLabel premierePhrase = new JLabel("Des valeurs manquantes (notées \"?\") ont été repérées dans les fichiers de données");
+    //Phrase du dÃ©but
+    JLabel premierePhrase = new JLabel("Des valeurs manquantes (notÃ©es \"?\") ont Ã©tÃ© repÃ©rÃ©es dans les fichiers de donnÃ©es");
     premierePhrase.setFont(new Font("Tahoma",Font.BOLD,14));
     premierePhrase.setForeground(Color.WHITE);
     JPanel premierPan = new JPanel();
@@ -36,19 +36,17 @@ public class BoiteDialogue extends JDialog implements ActionListener{
     
     // Boutons radio
     JPanel panContour = new JPanel();
-    //Font contour = new Font("Tahoma",Font.PLAIN,12);
     panContour.setBorder(BorderFactory.createTitledBorder(null, "  Comment voulez-vous traiter les valeurs manquantes ?", TitledBorder.LEADING, TitledBorder.DEFAULT_POSITION, new Font("Tahoma",Font.BOLD,11), Color.WHITE));
-    //panContour.setBorder(BorderFactory.createTitledBorder("Comment voulez-vous traiter les valeurs manquantes ?"));
-    choix1 = new JRadioButton("Considérer que les ? sont des valeurs à prendre en compte au même titre que les autres");
-    choix1.setSelected(true);
-    choix2 = new JRadioButton("Oublier les ? : l'algorithme se basera sur les valeurs non manquantes pour prédire la classe");
+    choix1 = new JRadioButton("ConsidÃ©rer que les ? sont des valeurs Ã  prendre en compte au mÃªme titre que les autres");
+    choix1.setSelected(true); // Le premier bouton radio est automatiquement sÃ©lectionnÃ©
+    choix2 = new JRadioButton("Oublier les ? : l'algorithme se basera sur les valeurs non manquantes pour prÃ©dire la classe");
     choix3 = new JRadioButton("Remplacer les ? par la valeur majoritaire de l'attribut correspondant");
-    ButtonGroup bg = new ButtonGroup(); // ButtonGroup nécessaire pour empêcher la sélection de plusieurs boutons radio
+    ButtonGroup bg = new ButtonGroup(); // ButtonGroup nÃ©cessaire pour empÃªcher la sÃ©lection de plusieurs boutons radio
     bg.add(choix1);
     bg.add(choix2);
     bg.add(choix3);
     JPanel panRadio = new JPanel();
-    panRadio.setLayout(new BoxLayout(panRadio, BoxLayout.Y_AXIS)); //Pour que les boutons soient organisés verticalement
+    panRadio.setLayout(new BoxLayout(panRadio, BoxLayout.Y_AXIS)); //Pour que les boutons soient organisÃ©s verticalement
     panRadio.add(choix1);
     panRadio.add(choix2);
     panRadio.add(choix3);
@@ -63,7 +61,6 @@ public class BoiteDialogue extends JDialog implements ActionListener{
     panContour.setBackground(Color.DARK_GRAY);
     
     //Image
-    //ImageIcon image = new ImageIcon("Data/pointInterrogation5.png");
     ImageIcon image = new ImageIcon("Data/questionMark.jpg");
     JLabel labImage = new JLabel(image);
     JPanel panImage = new JPanel();
@@ -78,27 +75,27 @@ public class BoiteDialogue extends JDialog implements ActionListener{
     //Bouton Valider
     JPanel bouton = new JPanel();
     JButton okBouton = new JButton("Valider");
-    okBouton.addActionListener(this);
+    okBouton.addActionListener(this); // Ajout d'un Ã©couteur
     bouton.add(okBouton);
     bouton.setBackground(Color.DARK_GRAY);
     
-    this.getContentPane().add(premierPan, BorderLayout.NORTH);
-    this.getContentPane().add(panFinal, BorderLayout.WEST);
-    this.getContentPane().add(bouton, BorderLayout.SOUTH);
-    this.getContentPane().add(panImage, BorderLayout.EAST);
-    this.setVisible(true);
+    this.getContentPane().add(premierPan, BorderLayout.NORTH); // Titre en haut
+    this.getContentPane().add(panFinal, BorderLayout.WEST); // Boutons radio Ã  gauche
+    this.getContentPane().add(bouton, BorderLayout.SOUTH); // Bouton de validation en bas
+    this.getContentPane().add(panImage, BorderLayout.EAST); // Image Ã  droite
+    this.setVisible(true); // On rend la boite de dialogue visible
   }
 
-	public void actionPerformed(ActionEvent arg0) {   
-		this.setVisible(false);
-		this.setChoix();
+	public void actionPerformed(ActionEvent arg0) { // Si on appuie sur le bouton
+		this.setVisible(false); // Fermeture de la boite de dialogue
+		this.setChoix(); // Enregistrement du choix dans this.choix
 	}
 	
 	public int getChoix() {
 		return this.choix;
 	}
   
-	public void setChoix(){
+	public void setChoix(){ // Enregistre le choix Ã©coutÃ© par le listener du bouton dans this.choix (1,2 ou 3)
 		if (choix1.isSelected()){
 			this.choix = 1;
 		} 
