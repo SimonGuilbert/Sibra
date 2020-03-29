fd1b76103fb4a1048c83f1bbd5b4cf101a3cdcc6
Simon Guilbert <simonpolytech@gmail.com>
2020-03-29 03:16:59 +0200

    Commentaires

--- ArbresDecision/Code/Main.java
+++ ArbresDecision/Code/Main.java
@@ -5,13 +5,13 @@ public class Main {
 		
 		// PARTIE APPRENTISSAGE
 		Lecture l = new Lecture("Data/soybean-app.csv");
-		Arbre a = new Arbre(new Noeud("NoeudRacine"),l.data(),l.choixArbre(),l.getChoixValManquantes()); // Création du noeud racine
-		a.setFils(); // Création du reste de l'arbre
+		Arbre a = new Arbre(new Noeud("NoeudRacine"),l.data(),l.choixArbre(),l.getChoixValManquantes()); // CrÃ©ation du noeud racine
+		a.setFils(); // CrÃ©ation du reste de l'arbre
 		Matrice matApp = new Matrice(a.getPredClasses(),a.listeDerniersElements(),a.listeClasses); //Transformation de 2 listes en matrice
 		
 		// PARTIE PREDICTION
 		l.setNomfichier("Data/soybean-pred.csv"); // On change de fichier
-		a.setDonnees(l.data()); // On change les données (nouvelles données = soybean-pred.csv)
+		a.setDonnees(l.data()); // On change les donnÃ©es (nouvelles donnÃ©es = soybean-pred.csv)
 		Matrice matPred = new Matrice(a.getPredClasses(),a.listeDerniersElements(),a.listeClasses); //Transformation de 2 listes en matrice
 		
 		// AFFICHAGE FENETRE
